9085d425b367f727845c93a3b809083f5456a1f1
Andrey Falkoskiy <avf-91@yandex.ru>
2026-07-20 21:12:33 +0300

    Что ниже Shared

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
 import {Footer} from "../../shared";
+import { Icon } from "../../assets/Icon/icon";
 
 export function Layout() {
     return <div>
         <div>Шапка</div>
+        <Icon.Check />
         <Outlet />
         <Footer />
     </div>
