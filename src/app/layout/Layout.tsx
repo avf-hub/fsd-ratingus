95e2634e045131ff30e2035143556f8c3df5c35a
Andrey Falkoskiy <avf-91@yandex.ru>
2026-07-20 13:20:06 +0300

    UI компоненты

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router";
+import {Footer} from "../../shared";
 
 export function Layout() {
     return <div>
         <div>Шапка</div>
         <Outlet />
-        <div>Футер</div>
+        <Footer />
     </div>
 }
